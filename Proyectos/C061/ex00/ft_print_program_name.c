483f4c2369d3113c3ba3277276d3d3e4f6a29cad
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2025-11-27 10:22:17 +0100

    Ejercicios piscina

---
+++ Proyectos/C061/ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vabad-ro <vabad-ro@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 12:04:56 by vabad-ro          #+#    #+#             */
+/*   Updated: 2025/11/26 14:16:36 by vabad-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	if (argc != 1)
+	{
+		write(2, "Error en el número de argumentos", 32);
+		return (1);
+	}
+	while (argv[0][i] != '\0')
+	{
+		write(1, &argv[0][i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
