189759fa5a2898c9763a1e6e8eb12fcb6c43ebee
VicProgram <victorabadromero@gmail.com>
2025-12-04 23:32:39 +0100

    yep

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -10,20 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include <stdio.h>
-*/
 
-void	ft_ft(int *nbr)
+void ft_ft(int *nbr)
 {
 	*nbr = 42;
 }
 
-/*
-int	main(void)
+int main(void)
 {
 	int c;
 	ft_ft(&c);
-	printf("%d", c);
+	printf("%d\n", c);
 }
-*/
