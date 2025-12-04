945617a36b530d44c158d75c6503a4d8e322a0ce
VicProgram <victorabadromero@gmail.com>
2025-12-04 21:13:27 +0100

    c06 ex03

--- C061/ex03/ft_sort_params.c
+++ C061/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: vabad-ro <vabad-ro@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 14:27:34 by vabad-ro          #+#    #+#             */
-/*   Updated: 2025/11/27 11:47:27 by vabad-ro         ###   ########.fr       */
+/*   Updated: 2025/12/04 21:30:00 by vabad-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,40 @@
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-	
+
 	i = 0;
-	while (s1[i] && s2[i])
-	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (s1[i] - s2[i]);
-	}
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
 	return (s1[i] - s2[i]);
 }
 
-void	ft_swap(char *s1, char *s2)
+void	ft_swap(char **s1, char **s2)
 {
 	char	*temp;
 
-	*temp = *s1;
+	temp = *s1;
 	*s1 = *s2;
-	*s2 = *s1;
+	*s2 = temp;
 }
 
-int	main(int argc,char **argv)
+int	main(int argc, char **argv)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	while (i < argc - 1) 
+	while (i < argc - 1)
 	{
-		if ((ft_strcmp(argv[i],  argv[i + 1]) >= 0))
-			ft_swap(argv[i] ,argv[i + 1]);
-		 i++;
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				ft_swap(&argv[i], &argv[j]);
+			j++;
+		}
+		i++;
 	}
+	i = 1;
 	while (i < argc)
 	{
 		j = 0;
@@ -56,7 +57,7 @@ int	main(int argc,char **argv)
 			write(1, &argv[i][j], 1);
 			j++;
 		}
-		write (1, "\n", 1);
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
