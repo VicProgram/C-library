59666f54bb4551d335ce8e30bcfc262468f9972e
VicProgram <victorabadromero@gmail.com>
2025-12-05 23:29:49 +0100

    ex01 c07

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	n;
+	int	j;
+	int	*arr;
+
+	if (min >= max)
+		return (NULL);
+	else
+		n = max - min;
+	
+	arr = malloc(n * sizeof(int));
+	if	(!arr)
+        return (0);
+	j = 0;
+	while (min < max)
+	{
+		arr[j] = min;
+		min++;
+		j++;
+	}
+	return (arr);
+}
+
+//pruebas con ayuda
+
+int main(void)
+{
+    int min = 4;
+    int max = 9;
+    int *arr;
+    int n;
+    int i;
+
+    arr = ft_range(min, max);
+    if (!arr)
+        return (0);
+
+    n = max - min;
+
+    i = 0;
+    while (i < n)
+    {
+        printf("%d ", arr[i]);
+        i++;
+    }
+
+    free(arr);
+	printf("\n");
+    return (0);
+}
