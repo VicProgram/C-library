acfa798b2fd8ec5ba9eed49edf154ca8cdc01b2c
VicProgram <victorabadromero@gmail.com>
2025-12-27 01:44:50 +0100

    vuelta

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,10 +6,29 @@
 /*   By: vic <vic@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 21:14:27 by vic               #+#    #+#             */
-/*   Updated: 2025/12/14 21:39:49 by vic              ###   ########.fr       */
+/*   Updated: 2025/12/27 01:44:40 by vic              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ultimate_range(void)
+int	ft_ultimate_range(int **range, int min, int max)
 {
+	int	res;
+	int	i;
+
+	//cuidado por que no puede valer negativo
+	res = max - min;
+	if (min >= max)
+	{
+		range = 0;
+		return ('c');
+	}
+
+	range = malloc(sizeof (int) * res);
+
+	i = 0;
+	while(i < res)
+	{
+		range[i] += min + i;
+		i++;
+	}
 }
